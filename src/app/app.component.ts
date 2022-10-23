8ce1042e2e3f5e29ff624a60641ded930b87f8b1
Harsha Narra <Harsha.Narra@ivycomptech.com>
2022-10-23 11:33:42 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'EvenBindingDemo';
+ 
+public val:any;
+  greet(event:any){
+    this.val = "hi Harsha";
+    
+  }
+  today: number = Date.now();
+  temp1:string='My name is Harsha';
+}
